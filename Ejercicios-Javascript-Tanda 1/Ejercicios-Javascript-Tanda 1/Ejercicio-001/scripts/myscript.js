2da11844c7dbda0b6bb9149d59c8c6e4815ee9ca
Jose Angel <ue137669@edu.xunta.gal>
2022-09-30 21:29:12 +0200

    primerCommit

---
+++ Ejercicios-Javascript-Tanda 1/Ejercicios-Javascript-Tanda 1/Ejercicio-001/scripts/myscript.js
@@ -0,0 +1,15 @@
+/***************************************************************************************************************
+*  
+*   Objetivo: Declarar el script mediante un fichero
+*             Mostrar en la consola el mensaje "Hola Mundo !"
+*             Mostrar en el documenho HTML el mensaje "Hola Mundo!"
+*
+*   Entrada : --
+*
+*
+*   Salida  : Hola Mundo !
+*
+*
+***************************************************************************************************************/
+console.log("Hola Mundo");
+document.write("Hola Mundo");
