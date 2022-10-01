2a5324064e58015f3bf101e762aa9aab95f9e1a3
Jose Angel <ue137669@edu.xunta.gal>
2022-10-01 22:07:32 +0200

    Ejercicio6

--- Ejercicios-Javascript-Tanda 1/Ejercicios-Javascript-Tanda 1/Ejercicio-006/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 1/Ejercicios-Javascript-Tanda 1/Ejercicio-006/scripts/myscript.js
@@ -10,3 +10,14 @@
 *
 *
 ***************************************************************************************************************/
+let catetoA;
+let catetoB;
+let hipotenusa;
+do {
+    catetoA = parseInt(prompt("Introduce el primer número: "));
+    catetoB = parseInt(prompt("Introduce el segundo número: "));
+} while (isNaN(catetoA && isNaN(catetoB)));
+
+hipotenusa = Math.sqrt(Math.pow(catetoA,2),Math.pow(catetoB,2));
+
+console.log(`La hipotenusa del triángulo con catetos ${catetoA} y ${catetoB} es ${hipotenusa}`);
