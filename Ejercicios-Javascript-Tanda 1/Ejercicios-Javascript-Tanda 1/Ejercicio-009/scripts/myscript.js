9438d6ab76a8e424db9c492da0857704e3800646
Jose Angel <ue137669@edu.xunta.gal>
2022-10-02 18:44:30 +0200

    Ejercicio9

--- Ejercicios-Javascript-Tanda 1/Ejercicios-Javascript-Tanda 1/Ejercicio-009/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 1/Ejercicios-Javascript-Tanda 1/Ejercicio-009/scripts/myscript.js
@@ -11,3 +11,14 @@
 *                   El número de caramelos que sobran es: YYYY
 *
 ***************************************************************************************************************/
+
+let nCaramelos;
+let nPeques;
+
+do {
+    nCaramelos = parseInt(prompt("Introduce el número de caramelos: "));
+    nPeques = parseInt(prompt("Introduce el número de peques: "));
+} while (isNaN(nCaramelos) || isNaN(nPeques) );
+
+console.log(`El número de caramelos por niño es: ${Math.trunc(nCaramelos / nPeques)}
+El número de ccaramelos que sobran es ${nCaramelos % nPeques}`);
