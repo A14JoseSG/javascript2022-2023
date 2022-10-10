5845bc1823bb8562e44aa1b1850108bc68ed757e
Jose Angel <ue137669@edu.xunta.gal>
2022-10-10 22:09:04 +0200

    Ejercicio20

--- Ejercicios-Javascript-Tanda 2/Ejercicio-020/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 2/Ejercicio-020/scripts/myscript.js
@@ -11,3 +11,37 @@
 *         ¿Cómo podemos hacerlo empleando arrays haciendo uso del método map? 
 *
 ***************************************************************************************************************/
+
+let inicio;
+let fin;
+let arrayNumeros = [];
+
+/*
+do {
+    inicio = parseInt(prompt("Introduce el primer número: "));
+    fin = parseInt(prompt("Introduce el segundo número: "));
+} while (isNaN(inicio) && isNaN(fin) || (inicio >= fin));
+
+for (let i = inicio; i <= fin; i++) {
+    console.log(Math.pow(i,2));
+    
+}*/
+
+/******* Con Arrays */
+
+function cuadrado( n){
+    return n*n;
+}
+
+do {
+    inicio = parseInt(prompt("Introduce el primer número: "));git 
+    fin = parseInt(prompt("Introduce el segundo número: "));
+} while (isNaN(inicio) && isNaN(fin) || (inicio >= fin));
+
+
+for (let i = inicio; i <=fin; i++) {
+     arrayNumeros.push(i);
+}
+
+
+console.log(arrayNumeros.map(cuadrado));
