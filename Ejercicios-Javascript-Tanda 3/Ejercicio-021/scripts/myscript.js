a722a2ccbe985c44fe00c563d08228a4e86b5452
a14josesg <a14josesg@iessanclemente.net>
2022-10-13 18:37:23 +0200

    tandas3y4add

---
+++ Ejercicios-Javascript-Tanda 3/Ejercicio-021/scripts/myscript.js
@@ -0,0 +1,19 @@
+/***************************************************************************************************************
+*  
+*   Objetivo: Mostrar los elementos comunes y no comunes de dos arrays de longitud n
+*             Crear tres funciones: 
+*                1.- Devuelve un array con los elementos comunes en los arrays
+*                2.- Devuelve los elementos del primer array que no están en el segundo array
+*                3.- Devuelve los elementos del segundo array que no están en el primer array
+*       
+*             Rellenar los arrays con númeos aleatorios entre -100 y 100
+*
+*   Entrada : La longitud del array (numero entero n)
+*
+*   Salida  : Los elementos del Array1
+*             Los elementos del Array2
+*             Los arrays tienen XX elementos comunes: .... (los elementos comunes separados por comas)
+*             Elementos del primer array no presentes en el segundo array: ...
+*             Elementos del segundo array no presentes en el primer array: ....
+*
+***************************************************************************************************************/
