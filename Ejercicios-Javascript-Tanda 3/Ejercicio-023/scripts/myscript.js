160f4f375a5125abffc1de3d26bde20526f72340
a14josesg <a14josesg@iessanclemente.net>
2022-10-20 19:15:24 +0200

    Ejercicio23

--- Ejercicios-Javascript-Tanda 3/Ejercicio-023/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 3/Ejercicio-023/scripts/myscript.js
@@ -17,3 +17,35 @@
 *
 *
 ***************************************************************************************************************/
+
+let n ;
+let cualificacion;
+
+do {
+    n = parseInt(prompt("Introduce la nota del examen: "));
+} while (isNaN(n) || (n < 0 || n>100));
+
+switch (true) {
+
+    case n < 60:
+        cualificacion = 'F';
+        break;
+    case n<70:
+        cualificacion = 'D';
+        break;
+    
+    case n < 80:
+        cualificacion = 'C'
+        break;
+
+    case n < 90:
+        cualificacion = 'B'
+        break;
+    
+    case n <= 100:
+        cualificacion = 'A'
+        break;
+
+}
+
+console.log(`La nota es ${cualificacion}`);
