8fbc1d0af9bdb0364b5957709e37e7155bc70289
a14josesg <a14josesg@iessanclemente.net>
2022-10-20 20:15:34 +0200

    Ejercicio-24

--- Ejercicios-Javascript-Tanda 3/Ejercicio-024/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 3/Ejercicio-024/scripts/myscript.js
@@ -10,3 +10,21 @@
 *
 *
 ***************************************************************************************************************/
+let n;
+
+
+do {
+    n = parseInt(prompt("Introduce un número "));
+} while (isNaN(n));
+
+const  cadena = Math.pow(n,2).toString();
+let salida=""
+
+//cadena.slice(-n.toString().length)==n.toString()
+if (cadena.endsWith(n)){
+    salida="Numero automorfico"
+} else {
+    salida="El número no es autmófico"
+}
+
+console.log(salida)
