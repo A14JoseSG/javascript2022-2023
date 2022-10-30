9ad9c631fc8cb168afd666c1f1a4b83d079ca9f3
Jose Angel <ue137669@edu.xunta.gal>
2022-10-30 12:33:41 +0100

    Ejercicio27

--- Ejercicios-Javascript-Tanda 3/Ejercicio-027/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 3/Ejercicio-027/scripts/myscript.js
@@ -11,3 +11,22 @@
 *
 *
 ***************************************************************************************************************/
+let nfilas = 0;
+let ncols = 0;
+
+do {
+    nfilas = parseFloat(prompt("Introduce el número de filas: "));
+    ncols = parseFloat(prompt("Introduce el número de columnas: "));
+} while ((isNaN(nfilas) || isNaN(ncols)) || (nfilas <1 || ncols <1));
+
+document.write("<table>");
+    for (let i = 1; i <= nfilas; i++) {
+        document.write("<tr>");
+            for (let j = 1; j <= nfilas; j++) {
+                document.write(`<td>${i*j}</td>`);
+                
+            }
+        document.write("</tr>");
+        
+    }
+document.write("</table>");
