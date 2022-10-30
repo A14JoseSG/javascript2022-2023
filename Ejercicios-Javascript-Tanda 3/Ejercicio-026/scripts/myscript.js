7eaa1451041bdb2c1daa02f0515498153150fbb2
Jose Angel <ue137669@edu.xunta.gal>
2022-10-30 12:13:30 +0100

    Ejercicio26

--- Ejercicios-Javascript-Tanda 3/Ejercicio-026/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 3/Ejercicio-026/scripts/myscript.js
@@ -16,3 +16,17 @@
 *
 *
 ***************************************************************************************************************/
+let n = 0;
+
+do {
+    n = parseInt(prompt("Introduce un número: "));
+} while (isNaN(n) || (n > 20 ||n< 1));
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        document.write(i," ");
+    }
+
+    document.write("<br>");
+
+}
