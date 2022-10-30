308f7216a3077882b1a2f3d2790e21de24a9ba1b
Jose Angel <ue137669@edu.xunta.gal>
2022-10-30 18:01:58 +0100

    Ejercicio29

--- Ejercicios-Javascript-Tanda 3/Ejercicio-029/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 3/Ejercicio-029/scripts/myscript.js
@@ -11,3 +11,17 @@
 *
 *
 ***************************************************************************************************************/
+let pandigital = true;
+let n= 0;
+let  arrayDigitos;
+const comprobarDigitos = ['0','1','2','3','4','5','6','7','8','9'];
+
+do {
+    n = prompt("Introduce un número: ");
+} while (isNaN(n));
+
+arrayDigitos = n.split("");
+
+   comprobarDigitos.forEach((el)=>  pandigital = arrayDigitos.includes(el));
+
+ (pandigital) ? console.log(`El numero ${n} es pandigital `) : console.log(`El numero ${n} no es pandigital`);
