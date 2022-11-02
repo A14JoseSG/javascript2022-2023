b8a4901e1a64dcf4f5ba7306d4ec6f181cf103d4
a14josesg <a14josesg@iessanclemente.net>
2022-11-02 20:14:42 +0100

    Ejercicio032

--- Ejercicios-Javascript-Tanda 4/Ejercicio-031/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 4/Ejercicio-031/scripts/myscript.js
@@ -11,3 +11,73 @@
 *
 *
 ***************************************************************************************************************/
+
+function pedirDato(msg, tipo) {
+    let centinela;
+    let dato;
+    do {
+      dato = prompt(msg);
+      if (tipo == "cadena") centinela = isNaN(dato) ? true : false;
+      if (tipo == "entero")
+        centinela =
+          !isNaN(dato) && Number.isInteger(parseFloat(dato)) ? true : false;
+      if (tipo == "flotante")
+        centinela =
+          !isNaN(dato) && !Number.isInteger(parseFloat(dato)) ? true : false;
+      if (tipo == "numero") centinela = !isNaN(dato) ? true : false;
+    } while (!centinela);
+    return dato;
+  }
+
+  function limpiarCaracter(caracter){
+    let caracterLimpio = caracter.toLowerCase();
+    switch (caracterLimpio) {
+        case "á":
+            caracterLimpio = "a";
+            break;
+        case "é":
+            caracterLimpio = "e";
+            break;
+        case "í":
+            caracterLimpio = "i";
+            break;
+        case "ú":
+            caracterLimpio = "u";
+            break;
+        case "ó":
+            caracterLimpio = "o";
+            break;        
+    }
+    return caracterLimpio;
+  }
+
+let cadena = pedirDato(`Introduce una cadena de texto: `, `cadena`);
+let resultado = {a:0, e:0, i:0, o:0, u:0};
+
+cadena = cadena.split("");
+
+cadena.forEach(element => {
+    switch (limpiarCaracter(element)) {
+        case 'a':
+            resultado["a"]++;
+            break;
+        case 'e':
+            resultado["e"]++;
+            break;
+        case 'i':
+            resultado["i"]++;
+            break;
+        case 'o':
+            resultado["o"]++;
+            break;
+        case 'u':
+            resultado["u"]++;
+            break;
+        
+    }
+});
+
+for (const clave in resultado) {
+        console.log(`El número de vocales es: ${clave} frecuencia ${resultado[clave]}`);
+    
+}
--- Ejercicios-Javascript-Tanda 4/Ejercicio-032/scripts/myscript.js
+++ Ejercicios-Javascript-Tanda 4/Ejercicio-032/scripts/myscript.js
@@ -11,3 +11,19 @@
 *
 *
 ***************************************************************************************************************/
+
+let array = [7,8,2,5,6,1,0,3,9];
+let perdido;
+
+array.sort((a,b)=>{
+    if(a==b) return 0;
+    if(a >b) return -1;
+    return 1;
+});
+
+for (let i = 0; i < array.length; i++) {
+    if(array[i] - array[i+1] == 2)perdido = array[i]-1;
+    
+}
+
+console.log(`El numero pedido es ${perdido}`)
